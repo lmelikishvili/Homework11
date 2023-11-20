044aea984879dd7cf2484fb602dd871b1aef8876
lmelikishvili <levanimelikishvili@gmail.com>
2023-11-20 20:02:19 +0400

    modify_searchbar

--- app/src/main/java/com/example/homework11/MainActivity.kt
+++ app/src/main/java/com/example/homework11/MainActivity.kt
@@ -2,27 +2,54 @@ package com.example.homework11
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView.LayoutManager
 import com.example.homework11.databinding.ActivityMainBinding
+import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private var animals = mutableListOf<Animal>()
+    private lateinit var search: SearchView
+    private var searchList = mutableListOf<Animal>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        search = binding.searchView
         setContentView(binding.root)
         setupAnimals()
 
-
         //binding.animalRecyclerView.layoutManager = LinearLayoutManager(this)
         binding.animalRecyclerView.layoutManager = GridLayoutManager(this,3)
-        binding.animalRecyclerView.adapter = AnimalRecyclerViewAdapter(animals = animals)
+        binding.animalRecyclerView.adapter = AnimalRecyclerViewAdapter(animals = searchList)
 
+        binding.searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                search.clearFocus()
+                return true
+            }
 
+            override fun onQueryTextChange(newText: String?): Boolean {
+                searchList.clear()
+                if (newText!!.isNotEmpty()){
+                    animals.forEach{
+                        if (it.title == newText){
+                            searchList.add(it)
+                        }
+                    }
+                    binding.animalRecyclerView.adapter!!.notifyDataSetChanged()
+                }else{
+                    searchList.clear()
+                    searchList.addAll(animals)
+                    binding.animalRecyclerView.adapter!!.notifyDataSetChanged()
+                }
+                return false
+            }
+        })
     }
+
     private fun setupAnimals(){
         animals.also {
             it.add(Animal("dog1", "dog1", R.drawable.dog1))
@@ -40,8 +67,7 @@ class MainActivity : AppCompatActivity() {
             it.add(Animal("dog13", "dog13", R.drawable.dog13))
             it.add(Animal("dog14", "dog14", R.drawable.dog14))
             it.add(Animal("dog15", "dog15", R.drawable.dog15))
-
-
         }
+        searchList.addAll(animals)
     }
 }
